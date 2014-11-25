4028ab5a619dbd5eba7f1734ac6ce906fadbc8b1
miumiu997 <miuyinyong@yahoo.com>
2014-11-25 11:47:29 -0600

    optimizar

--- analisis.asm
+++ analisis.asm
@@ -18,6 +18,15 @@
 	dos: db "dos",0
 	tres: db "tres",0 
 =======
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+	uno: db "uno",0
+	dos: db "dos",0
+	tres: db "tres",0 
+=======
+>>>>>>> FETCH_HEAD
+>>>>>>> Stashed changes
 >>>>>>> FETCH_HEAD
   	msg_size3: equ $-msg3
   	tab: db  9 ;ASCII for vertical tab
@@ -60,9 +69,20 @@ main:
 	call matriz
 	call setDatosFor1
 =======
+<<<<<<< Updated upstream
 	nwl
 	call matriz
 >>>>>>> FETCH_HEAD
+=======
+<<<<<<< HEAD
+	call matriz
+	call setDatosFor1
+=======
+	nwl
+	call matriz
+>>>>>>> FETCH_HEAD
+>>>>>>> FETCH_HEAD
+>>>>>>> Stashed changes
 	call salir
 
 leeArch1:
@@ -101,6 +121,178 @@ leeArch2:
 	mov [archivo2], ECX 
 	mov [TamArch2], EAX	
 <<<<<<< HEAD
+<<<<<<< Updated upstream
+	ret
+matriz:
+	mov AX, [TamArch1]
+	mov BX, [TamArch2]
+	inc AX
+	inc BX
+	mov word[m], AX
+  	mov word[n], BX    
+  	mov eax, 0
+  	mov ebx, matrix1  
+  	mov word[i], 0
+  	mov word[j], 0
+	call i_loop
+	ret
+  
+  
+i_loop:
+    	mov word[j], 0
+	call j_loop
+	ret
+j_loop:
+ 	mov dx , 1
+ 	mov  word[ebx + 2 * eax], dx
+ 	inc eax    ;Incrementing array index by one....
+ 	inc word[j]
+ 	mov cx, word[j]
+ 	cmp cx, word[n]
+ 	jb j_loop
+    	inc word[i]
+    	mov cx, word[i]
+    	cmp cx, word[m]
+    	jb i_loop
+	mov eax, 0
+  	mov ebx, matrix1  
+  	mov word[i], 0
+  	mov word[j], 0
+	call i_loop2
+	ret
+i_loop2:
+    	mov word[j], 0
+	call j_loop2
+	ret
+j_loop2:
+    	mov  dx, word[ebx + 2 * eax]
+   	mov word[num] , dx
+   	call print_num
+   	pusha
+     	mov eax, 4
+     	mov ebx, 1
+     	mov ecx, tab
+     	mov edx, 1
+     	int 80h    
+   	popa
+   	inc eax  
+	inc word[j]
+ 	mov cx, word[j]
+ 	cmp cx, word[n]
+ 	jb j_loop2
+   	pusha
+     	mov eax, 4
+     	mov ebx, 1
+     	mov ecx, new_line
+     	mov edx, 1
+     	int 80h    
+   	popa
+    	inc word[i]
+    	mov cx, word[i]
+    	cmp cx, word[m]
+    	jb i_loop2
+	ret
+
+exit:
+  	mov eax, 1
+  	mov ebx, 0
+  	int 80h  
+loop_read:
+    	mov eax, 3
+    	mov ebx, 0
+    	mov ecx, temp
+    	mov edx, 1
+    	int 80h
+    	cmp byte[temp], 10
+    	je end_read   
+    	mov ax, word[num]
+    	mov bx, 10
+    	mul bx
+    	mov bl, byte[temp]
+    	sub bl, 30h
+    	mov bh, 0
+    	add ax, bx
+    	mov word[num], ax
+    	jmp loop_read 
+end_read:
+  	popa
+  	ret
+print_num:
+  	pusha
+extract_no:
+    	cmp word[num], 0
+    	je print_no
+    	inc byte[nod]
+    	mov dx, 0
+    	mov ax, word[num]
+    	mov bx, 10
+    	div bx
+    	push dx
+    	mov word[num], ax
+    	jmp extract_no
+
+print_no:
+    	cmp byte[nod], 0
+    	je end_print
+    	dec byte[nod]
+    	pop dx
+    	mov byte[temp], dl
+    	add byte[temp], 30h
+    	mov eax, 4
+    	mov ebx, 1
+    	mov ecx, temp
+    	mov edx, 1
+    	int 80h
+    	jmp print_no    
+end_print:   
+  	popa
+  	ret 
+
+setDatosFor1:
+	xor ECX,ECX
+	xor EAX,EAX
+	mov EAX,10
+	mov EDI,0
+	mov ESI,2
+	call inicializarMatriz
+	ret
+
+
+inicializarMatriz:
+	dec eax
+	mov ECX,5
+	call inicializaMatriz2
+	;inc edi      ;i
+	cmp eax,0
+	jne inicializarMatriz
+	ret
+inicializaMatriz2:
+	dec ecx
+	cmp edi,0
+	je ifuno
+	cmp esi,0
+	je ifdos
+	jmp iftres
+	inc esi      ;j
+	cmp ecx,0
+	jne inicializaMatriz2
+	ret	
+
+ifuno:
+	PutStr uno
+	nwln 
+	ret
+ifdos:
+	PutStr dos 
+	nwln
+	ret
+iftres:
+	PutStr tres
+	nwln
+=======
+>>>>>>> FETCH_HEAD
+=======
+>>>>>>> Stashed changes
 	ret
 matriz:
 	mov AX, [TamArch1]
@@ -274,157 +466,115 @@ iftres:
 matriz:
 	mov AX, [TamArch1]
 	mov BX, [TamArch2]
+	inc AX
+	inc BX
 	mov word[m], AX
   	mov word[n], BX    
+  	mov eax, 0
+  	mov ebx, matrix1  
+  	mov word[i], 0
+  	mov word[j], 0
+	call i_loop
+	ret
   
   
-  ;Reading each element of the matrix........
-  mov eax, 0
-  mov ebx, matrix1  
-  
-  mov word[i], 0
-  mov word[j], 0
-  
-  
-  i_loop:
-    mov word[j], 0
-    j_loop:
- 
- mov dx , 1
- ;eax will contain the array index and each element is 2 bytes(1 word) long
- mov  word[ebx + 2 * eax], dx
- inc eax    ;Incrementing array index by one....
-    
- inc word[j]
- mov cx, word[j]
- cmp cx, word[n]
- jb j_loop
- 
-    inc word[i]
-    mov cx, word[i]
-    cmp cx, word[m]
-    jb i_loop
-;Reading each element of the matrix.(Storing the elements in row major order).......
-  mov eax, 0
-  mov ebx, matrix1  
-  
-  mov word[i], 0
-  mov word[j], 0
-  
-  
-  i_loop2:
-    mov word[j], 0
-    j_loop2:
- 
-   ;eax will contain the array index and each element is 2 bytes(1 word) long
-   mov  dx, word[ebx + 2 * eax]   ;
-   mov word[num] , dx
-   call print_num
-   
-   ;Printing a space after each element.....
-   pusha
-     mov eax, 4
-     mov ebx, 1
-     mov ecx, tab
-     mov edx, 1
-     int 80h    
-   popa
-   
-   inc eax  
-     
-     
- inc word[j]
- mov cx, word[j]
- cmp cx, word[n]
- jb j_loop2
- 
-  pusha
-     mov eax, 4
-     mov ebx, 1
-     mov ecx, new_line
-     mov edx, 1
-     int 80h    
-   popa
- 
-    inc word[i]
-    mov cx, word[i]
-    cmp cx, word[m]
-    jb i_loop2
-  
+i_loop:
+    	mov word[j], 0
+	call j_loop
+	ret
+j_loop:
+ 	mov dx , 1
+ 	mov  word[ebx + 2 * eax], dx 
+ 	inc eax    ;Incrementing array index by one....
+ 	inc word[j]
+ 	mov cx, word[j]
+ 	cmp cx, word[n]
+ 	jb j_loop
+    	inc word[i]
+    	mov cx, word[i]
+    	cmp cx, word[m]
+    	jb i_loop
+	mov eax, 0
+  	mov ebx, matrix1  
+  	mov word[i], 0
+  	mov word[j], 0
+	call i_loop2
+	ret
+i_loop2:
+    	mov word[j], 0
+	call j_loop2
+	ret
+j_loop2:
+    	mov  dx, word[ebx + 2 * eax]
+   	mov word[num] , dx
+   	call print_num
+   	pusha
+     	mov eax, 4
+     	mov ebx, 1
+     	mov ecx, tab
+     	mov edx, 1
+     	int 80h    
+   	popa
+   	inc eax  
+	inc word[j]
+ 	mov cx, word[j]
+ 	cmp cx, word[n]
+ 	jb j_loop2
+   	pusha
+     	mov eax, 4
+     	mov ebx, 1
+     	mov ecx, new_line
+     	mov edx, 1
+     	int 80h    
+   	popa
+    	inc word[i]
+    	mov cx, word[i]
+    	cmp cx, word[m]
+    	jb i_loop2
+	ret
 
-  
-  
-
-  
-;Exit System Call.....
-exit:
-  mov eax, 1
-  mov ebx, 0
-  int 80h
-
-  
-  
-;Function to read a number from console and to store that in num 
-read_num:
-  pusha
-  mov word[num], 0
-  
-loop_read:
-    mov eax, 3
-    mov ebx, 0
-    mov ecx, temp
-    mov edx, 1
-    int 80h
-    cmp byte[temp], 10
-    je end_read   
-    mov ax, word[num]
-    mov bx, 10
-    mul bx
-    mov bl, byte[temp]
-    sub bl, 30h
-    mov bh, 0
-    add ax, bx
-    mov word[num], ax
-    jmp loop_read 
-end_read:
-  popa
-  ret
+setDatosFor1:
+	xor ECX,ECX
+	xor EAX,EAX
+	mov EAX,10
+	mov EDI,0
+	mov ESI,2
+	call inicializarMatriz
+	ret
 
 
-;Function to print any number stored in num...
-print_num:
-  pusha
-extract_no:
-    cmp word[num], 0
-    je print_no
-    inc byte[nod]
-    mov dx, 0
-    mov ax, word[num]
-    mov bx, 10
-    div bx
-    push dx
-    mov word[num], ax
-    jmp extract_no
+inicializarMatriz:
+	dec eax
+	mov ECX,5
+	call inicializaMatriz2
+	;inc edi      ;i
+	cmp eax,0
+	jne inicializarMatriz
+	ret
+inicializaMatriz2:
+	dec ecx
+	cmp edi,0
+	je ifuno
+	cmp esi,0
+	je ifdos
+	jmp iftres
+	inc esi      ;j
+	cmp ecx,0
+	jne inicializaMatriz2
+	ret	
 
-print_no:
-    cmp byte[nod], 0
-    je end_print
-    dec byte[nod]
-    pop dx
-    mov byte[temp], dl
-    add byte[temp], 30h
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, temp
-    mov edx, 1
-    int 80h
-    jmp print_no
-    
-end_print:   
-  popa
-  ret 
-    
- 	
+ifuno:
+	PutStr uno
+	nwln 
+	ret
+ifdos:
+	PutStr dos 
+	nwln
+	ret
+iftres:
+	PutStr tres
+	nwln
+	ret
 salir:
 	.EXIT
 
